3d0e3b97bbc82866c1d199bb1fe1c64dbefe4900
Lazzlo2096 <lazzlo2096@yandex.ru>
2022-08-30 17:44:42 +0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -12,28 +12,30 @@
 program3 = "(def x 42) (+ x 8)"
 
 --------
-{-
-data Arithms = Plus Ar2 Ar2 -- не робит
-data Ar2 = Arithms | Integer
--}
-data Arithms =
-    Plus Arithms Integer
-  | Plus2 Integer Integer
-  | Plus3 Integer Arithms
-  | Plus4 Arithms Arithms
--- Не, это не правильно ваще. Plus должен быть как значение, а не как парсинг.
+
+data SExpr = A [SExpr]
+           | B String
+           | C Integer
+
+
+-- data [a]      = (:)  a [a]        | []
+-- data []     a = (:)  a ([] a)     | []
+-- data MyList a = Cons a (MyList a) | Nil
+
+
 
 program2 = "(+ (+ 2 3) 8)"
 
-s2 = Plus (Plus2 2 3) 8
+
+s2 = A [ B "+", A [ B "+", B "2", B "3"], B "8" ]    -- "(+ (+ 2 3) 8)"
+s3 = A [ B "doThing" ]                              -- "(doThing )"
+s4 = A [ B "doThing", A [B "1" , B "2", B "3"] ]    -- "(doThing 1 2 3)"
 
 --------
--- data Arithms = Plus Integer Integer
 
 program1 = "(+ 1 2)"
+s1 = A [B "+", C 1, C 2]
 
--- s1 = parseMyLisp program1    -- progInHaskellTypes
-s1 = Plus2 1 2
 
 -- eval :: Arithms -> Integer
 -- eval (Plus a b) = (+) a b
