dedbb885294eab90c81466e5cec44bd3677db2a2
Lazzlo2096 <lazzlo2096@yandex.ru>
2022-08-30 23:24:42 +0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,66 +1,71 @@
---------
---parser:
--- "(" s_expr ")"
--- s_expr = name | "+" <> number <> number => Plus (toInt $2) (toInt $3)
---
--- (def x 42) => m::Map String Value ; m.add("x", "42");
--- Not multy map. -- Hash? Or Tree?
---
---
---------
-
-program3 = "(def x 42) (+ x 8)"
-
---------
+import Data.Maybe
+import Data.List
 
 data LispVal = Atom String
            | List [LispVal]
            | String String
            | Number Integer
-    
+           -- deriving Show
+
+instance Show LispVal where
+  show = showVal
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number x) = show x
+showVal (List xs) = "(" ++ (mconcat (intersperse " " (map showVal xs))) ++ ")"
 
 
 -- data [a]      = (:)  a [a]        | []
 -- data []     a = (:)  a ([] a)     | []
 -- data MyList a = Cons a (MyList a) | Nil
 
-
-
+--------
+program3 = "(def x 42) (+ x 8)"
 program2 = "(+ (+ 2 3) 8)"
 
-
-s2 = List [ Atom "+", List [ Atom "+", Number 2, Number 3], Number 8 ]    -- "(+ (+ 2 3) 8)"
-
-
---------
+-- "(+ (+ 2 3) 8)"
+s2 = List [ Atom "+", List [ Atom "+", Number 2, Number 3], Number 8 ]    
 
 program1 = "(+ 1 2)"
 s1 = List [Atom "+", Number 1, Number 2]
+
 s12 = Number 2
+--------
 
-eval :: LispVal -> Integer
-eval (Number x)     = x
--- eval (Atom x)       = -- ??
--- eval (List (x:xs))  = eval x + eval xs -- ??
-eval (List [Atom "+", Number a, Number b]) = (+) a b
-{-
-eval (Atom x) = case x of
-            "+" -> evalWithOp (eval ?? ) (+)
-            "-" -> evalWithOp (eval ?? ) (-)
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+-- eval val@(Bool _) = val
+-- eval (List [Atom "quote", val]) = val
 
-evalWithOp :: LispVal -> (a -> b) -> Integer
-evalWithOp a op = 
--}
+eval (List (Atom func : args)) = apply func (map eval args)
 
+apply :: String -> [LispVal] -> LispVal
+apply func args  = fromJust (lookup func primitives) $ args
 
-ops = [
-  ( "+", (+) ),
-  ( "-", (-) )
+primitives = [
+  ( "+", numericBinop (+) ),
+  ( "-", numericBinop (-) )
   ]
+
+
+-- "(+ 1 2 3)"
+-- [Number 1, Number 2, Number 3]
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (List [n]) = unpackNum n
+-- unpackNum (String n) = 
+-- unpackNum _ = 0
+
 --------
 
 main = do
   putStrLn "Hello World"
 
-  print $ eval s12 == 2
-  print $ eval s1  == 3
+  print $ eval s2
+  --print $ eval s1  == 3
