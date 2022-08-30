ecfe6c601e3f62f0b86a59deb3324a73ce49e8c7
Lazzlo2096 <lazzlo2096@yandex.ru>
2022-08-30 17:22:28 +0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,13 +1,46 @@
-programm1 = "(+ 1 2)"
+--------
+--parser:
+-- "(" s_expr ")"
+-- s_expr = name | "+" <> number <> number => Plus (toInt $2) (toInt $3)
+--
+-- (def x 42) => m::Map String Value ; m.add("x", "42");
+-- Not multy map. -- Hash? Or Tree?
+--
+--
+--------
 
-data Arithms = Plus Integer Integer
+program3 = "(def x 42) (+ x 8)"
 
-s1 = Plus 1 2
+--------
+{-
+data Arithms = Plus Ar2 Ar2 -- не робит
+data Ar2 = Arithms | Integer
+-}
+data Arithms =
+    Plus Arithms Integer
+  | Plus2 Integer Integer
+  | Plus3 Integer Arithms
+  | Plus4 Arithms Arithms
+-- Не, это не правильно ваще. Plus должен быть как значение, а не как парсинг.
 
-eval :: Arithms -> Integer
-eval (Plus a b) = (+) a b
+program2 = "(+ (+ 2 3) 8)"
+
+s2 = Plus (Plus2 2 3) 8
+
+--------
+-- data Arithms = Plus Integer Integer
+
+program1 = "(+ 1 2)"
+
+-- s1 = parseMyLisp program1    -- progInHaskellTypes
+s1 = Plus2 1 2
+
+-- eval :: Arithms -> Integer
+-- eval (Plus a b) = (+) a b
+
+--------
 
 main = do
   putStrLn "Hello"
   putStrLn "World"
-  putStrLn $ show $ (eval s1) == 3
+  -- print $ eval s1 == 3
